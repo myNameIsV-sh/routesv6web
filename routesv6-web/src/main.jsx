de4a5e930cdcd08dceed091ad0d6ba867dc50bef
V <victor.gustavo.c@proton.me>
2025-08-26 20:55:56 -0300

    Adicionando a primeira rota

--- routesv6-web/src/main.jsx
+++ routesv6-web/src/main.jsx
@@ -2,13 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Root from './routes/root.jsx';
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Root />,
     },
 ]);
 
