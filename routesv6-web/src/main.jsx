a3273495b986c601b1f6146b015978b0a68d2a89
V <victor.gustavo.c@proton.me>
2025-08-26 21:04:08 -0300

    Tornando a rota uma criança da rota raiz

--- routesv6-web/src/main.jsx
+++ routesv6-web/src/main.jsx
@@ -13,11 +13,14 @@ const router = createBrowserRouter([
         path: "/",
         element: <Root />,
         errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "contacts/:contactId",
+                element: <Contact />,
+            },
+        ],
     },
-    {
-        path: "contacts/:contactId",
-        element: <Contact />,
-    },
+
 ]);
 
 createRoot(document.getElementById('root')).render(
