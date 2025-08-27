ae5948c0c6b78444134e083f05fdc76b047d3775
V <victor.gustavo.c@proton.me>
2025-08-26 21:40:58 -0300

    Inutilizando o App.jsx

--- routesv6-web/src/main.jsx
+++ routesv6-web/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Root from './routes/root.jsx';
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
